f7b7b584d4a1e73e2f3273d38f27d8fad47e6def
vicky1997 <vickyvaikunth@gmail.com>
2025-03-20 19:02:38 +0100

    index page works

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'mcdust'
+copyright = '2025, Vaikundaraman'
+author = 'Vaikundaraman'
 
 release = '0.1'
 version = '0.1.0'
